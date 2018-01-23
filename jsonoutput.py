3a0fbf64ec794904df14cadb63c9a21cbcd2e10e
Francisco Beltrao <francisco.beltrao@outlook.com>
2018-01-23 11:55:45 +0100

    Create jsonoutput.py

---
+++ jsonoutput.py
@@ -0,0 +1,18 @@
+from Adafruit_BME280 import *
+import calendar
+import time
+import socket
+
+sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+
+degrees = sensor.read_temperature()
+pascals = sensor.read_pressure()
+hectopascals = pascals / 100
+humidity = sensor.read_humidity()
+
+timestamp = calendar.timegm(time.gmtime())
+
+print '{{ timestamp: {3}, device: {4}, temp: {0:0.3f}, pressure: {1:0.2f}, humidity: {2:0.2f} }}'.format(degrees, hectopascals, humidity, timestamp, socket.gethostname())
+print 'Temp      = {0:0.3f} deg C'.format(degrees)
+print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
+print 'Humidity  = {0:0.2f} %'.format(humidity)
